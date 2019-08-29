865eca44da98846d8afb3c93916ce3ce398adf52
Fabien Mercier <fmercier@octo.com>
2019-08-29 15:59:30 +0200

    (pC-2235) humanize product_id

--- recommendations_engine/recommendations.py
+++ recommendations_engine/recommendations.py
@@ -22,16 +22,18 @@ def give_requested_recommendation_to_user(user, offer_id, mediation_id):
     recommendation = None
 
     if mediation_id or offer_id:
-        recommendation = _find_recommendation(offer_id, mediation_id, user.id)
+        recommendation = _create_or_find_recommendation_already_created_on_discovery(
+            offer_id, mediation_id, user.id)
         if recommendation is None:
-            recommendation = _create_recommendation_from_ids(user, offer_id, mediation_id=mediation_id)
-            logger.debug(lambda: 'Creating Recommendation with offer_id=%s mediation_id=%s' % (offer_id, mediation_id))
+            recommendation = _create_recommendation_from_ids(
+                user, offer_id, mediation_id=mediation_id)
+            logger.debug(lambda: 'Creating Recommendation with offer_id=%s mediation_id=%s' % (
+                offer_id, mediation_id))
 
     return recommendation
 
 
 def create_recommendations_for_discovery(limit=3, user=None):
-
     recommendations = []
     tuto_mediations_by_index = {}
 
@@ -53,9 +55,9 @@ def create_recommendations_for_discovery(limit=3, user=None):
         while read_recommendations_count + index + inserted_tuto_mediations \
                 in tuto_mediations_by_index:
             tuto_mediation_index = read_recommendations_count \
-                                        + index \
-                                        + inserted_tuto_mediations
-            create_tuto_mediation_if_non_existent_for_user(
+                + index \
+                + inserted_tuto_mediations
+            _create_tuto_mediation_if_non_existent_for_user(
                 user,
                 tuto_mediations_by_index[tuto_mediation_index]
             )
@@ -64,7 +66,7 @@ def create_recommendations_for_discovery(limit=3, user=None):
     return recommendations
 
 
-def create_tuto_mediation_if_non_existent_for_user(user, tuto_mediation):
+def _create_tuto_mediation_if_non_existent_for_user(user, tuto_mediation):
 
     already_existing_tuto_recommendation = Recommendation.query\
         .filter_by(mediation=tuto_mediation, user=user)\
@@ -83,10 +85,12 @@ def _no_mediation_or_mediation_does_not_match_offer(mediation, offer_id):
     return mediation is None or (offer_id and (mediation.offerId != offer_id))
 
 
-def _find_recommendation(offer_id, mediation_id, user_id):
-    logger.debug(lambda: 'Requested Recommendation with offer_id=%s mediation_id=%s' % (offer_id, mediation_id))
+def _create_or_find_recommendation_already_created_on_discovery(offer_id, mediation_id, user_id):
+    logger.debug(lambda: 'Requested Recommendation with offer_id=%s mediation_id=%s' % (
+        offer_id, mediation_id))
     query = Recommendation.query.filter((Recommendation.validUntilDate > datetime.utcnow())
-                                        & (Recommendation.userId == user_id))
+                                        & (Recommendation.userId == user_id)
+                                        & (Recommendation.search == None))
     if offer_id:
         query = query.join(Offer)
     mediation = Mediation.query.filter_by(id=mediation_id).first()
@@ -95,14 +99,15 @@ def _find_recommendation(offer_id, mediation_id, user_id):
     if mediation_id:
         if _no_mediation_or_mediation_does_not_match_offer(mediation, offer_id):
             logger.debug(lambda: 'Mediation not found or found but not matching offer for offer_id=%s mediation_id=%s'
-                        % (offer_id, mediation_id))
+                         % (offer_id, mediation_id))
             raise OfferNotFoundException()
 
         query = query.filter(Recommendation.mediationId == mediation_id)
 
     if offer_id:
         if offer is None:
-            logger.debug(lambda: 'Offer not found for offer_id=%s' % (offer_id,))
+            logger.debug(lambda: 'Offer not found for offer_id=%s' %
+                         (offer_id,))
             raise OfferNotFoundException()
 
         query = query.filter(Offer.id == offer_id)
@@ -153,13 +158,13 @@ def _create_recommendation(user, offer, mediation=None):
     return recommendation
 
 
-def get_search(kwargs):
+def _get_search(kwargs):
     return '&'.join([key + '=' + str(value) for (key, value) in kwargs.items()])
 
 
 def create_recommendations_for_search(user, **kwargs):
     offers = get_offers_for_recommendations_search(**kwargs)
-    search = get_search(kwargs)
+    search = _get_search(kwargs)
 
     recommendations = []
 
@@ -184,11 +189,13 @@ def get_recommendation_search_params(request_args: dict) -> dict:
 
     if 'categories' in request_args and request_args['categories']:
         type_sublabels = request_args['categories']
-        search_params['type_values'] = get_event_or_thing_type_values_from_sublabels(type_sublabels)
+        search_params['type_values'] = get_event_or_thing_type_values_from_sublabels(
+            type_sublabels)
 
     if 'date' in request_args and request_args['date']:
         date = dateutil.parser.parse(request_args['date'])
-        search_params['days_intervals'] = [[ date, date + timedelta(days=int(1)) ]]
+        search_params['days_intervals'] = [
+            [date, date + timedelta(days=int(1))]]
 
     if 'days' in request_args and request_args['days']:
         date = dateutil.parser.parse(request_args['date'])
